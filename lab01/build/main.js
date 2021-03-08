518870c8e27c1cfee3cf3f43112a0b4bf8762991
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-03-08 20:08:49 +0100

    Dodanie generowania inputów

--- lab01/build/main.js
+++ lab01/build/main.js
@@ -1,14 +1,44 @@
 "use strict";
 var Main = /** @class */ (function () {
     function Main() {
+        this.inputManager = new InputManager();
     }
-    Main.run = function () {
-        var im = new InputManager();
+    Main.prototype.run = function () {
+        this.setupSubmitButton();
+    };
+    Main.prototype.setupSubmitButton = function () {
+        var _this = this;
+        var _a;
+        (_a = document.querySelector('#add-input')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function (event) {
+            _this.onSubmitClicked(event);
+        });
+    };
+    Main.prototype.onSubmitClicked = function (event) {
+        var _a, _b;
+        console.log('xd');
+        var numberOfInputsInput = document.querySelector('#number-of-new-inputs');
+        var numberOfInputs = parseInt((_a = numberOfInputsInput === null || numberOfInputsInput === void 0 ? void 0 : numberOfInputsInput.value) !== null && _a !== void 0 ? _a : '0');
+        for (var i = 0; i < numberOfInputs; i++) {
+            var div = document.createElement('div');
+            var input = document.createElement('input');
+            input.type = 'number';
+            input.classList.add('input-field');
+            div.appendChild(input);
+            this.inputManager.add(input);
+            (_b = document.querySelector('#inputs-wrapper')) === null || _b === void 0 ? void 0 : _b.appendChild(div);
+        }
+        if (numberOfInputsInput != null) {
+            numberOfInputsInput.value = '';
+        }
     };
     return Main;
 }());
 var InputManager = /** @class */ (function () {
     function InputManager() {
+        this.inputs = [];
+        this.loadInputs();
+    }
+    InputManager.prototype.loadInputs = function () {
         var _this = this;
         this.inputs = [];
         document.querySelectorAll('.input-field').forEach(function (el) {
@@ -16,7 +46,12 @@ var InputManager = /** @class */ (function () {
             typedEl.addEventListener('input', function () { return _this.onInputChanged(); });
             _this.inputs.push(typedEl);
         });
-    }
+    };
+    InputManager.prototype.add = function (input) {
+        var _this = this;
+        input.addEventListener('input', function () { return _this.onInputChanged(); });
+        this.inputs.push(input);
+    };
     InputManager.prototype.getSum = function () {
         var sum = 0;
         this.inputs.forEach(function (el) {
@@ -69,4 +104,7 @@ var InputManager = /** @class */ (function () {
     };
     return InputManager;
 }());
-Main.run();
+document.addEventListener('DOMContentLoaded', function () {
+    var main = new Main();
+    main.run();
+});
--- lab01/src/main.ts
+++ lab01/src/main.ts
@@ -1,19 +1,62 @@
 class Main {
-    static run() {
-        new InputManager();
+    private inputManager: InputManager;
+
+    constructor() {
+        this.inputManager = new InputManager();
+    }
+
+    run() {
+        this.setupSubmitButton();
+    }
+
+    private setupSubmitButton() {
+        document.querySelector('#add-input')?.addEventListener('click', (event) => {
+            this.onSubmitClicked(event);
+        });
+    }
+
+    private onSubmitClicked(event: Event) {
+        console.log('xd');
+
+        const numberOfInputsInput = document.querySelector<HTMLInputElement>('#number-of-new-inputs');
+        const numberOfInputs = parseInt(numberOfInputsInput?.value ?? '0');
+
+        for (let i = 0; i < numberOfInputs; i++) {
+            const div = document.createElement('div');
+            const input = document.createElement('input');
+            input.type = 'number';
+            input.classList.add('input-field');
+            div.appendChild(input);
+
+            this.inputManager.add(input);
+            document.querySelector('#inputs-wrapper')?.appendChild(div);
+        }
+
+        if (numberOfInputsInput != null) {
+            numberOfInputsInput.value = '';
+        }
     }
 }
 
 class InputManager {
-    private inputs: HTMLInputElement[];
+    private inputs: HTMLInputElement[] = [];
 
     constructor() {
+        this.loadInputs();
+    }
+
+    private loadInputs() {
         this.inputs = [];
         document.querySelectorAll('.input-field').forEach((el) => {
             const typedEl = el as HTMLInputElement;
             typedEl.addEventListener('input', () => this.onInputChanged());
             this.inputs.push(typedEl);
         })
+    }
+
+    add(input: HTMLInputElement) {
+        input.addEventListener('input', () => this.onInputChanged());
+        this.inputs.push(input);
     }
 
     getSum(): number {
@@ -79,4 +122,7 @@ class InputManager {
     }
 }
 
-Main.run();
+document.addEventListener('DOMContentLoaded', () => {
+    const main = new Main();
+    main.run();
+});
